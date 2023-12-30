3eaaabada2bbb02c87ee752afcd62d079970fbc1
D <dewandrarb@gmail.com>
2023-12-30 08:57:28 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="flex justify-center items-center h-screen font-myFont">
+        <p className="text-3xl font-extrabold">THIS WEBSITE IS CURRENTLY UNDER REPAIR!!!</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
