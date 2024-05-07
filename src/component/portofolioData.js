a0ff832763c373696d804f15e858dc985d206ec1
D <dewandrarb@gmail.com>
2024-05-07 17:02:30 +0700

    under maintanance

--- src/component/portofolioData.js
+++ src/component/portofolioData.js
@@ -1,19 +1,19 @@
 const portfolioData = [
-  {
-    id: 1,
-    url: "https://www.youtube.com/embed/9QHMsB4wAAs?si=polRgbezBH8Q61aO",
-    title: "Profile Website",
-  },
-  {
-    id: 2,
-    url: "https://www.youtube.com/embed/jd_lEsHDirM?si=P8PiFDyHi5gDiivF",
-    title: "Persuratan Digital",
-  },
-  {
-    id: 2,
-    url: "https://www.youtube.com/embed/J84ZTPegk5U?si=Y_PDIJ6W5yrplZm8",
-    title: "Bakorwil III Malang",
-  },
+  // {
+  //   id: 1,
+  //   url: "https://www.youtube.com/embed/9QHMsB4wAAs?si=polRgbezBH8Q61aO",
+  //   title: "Profile Website",
+  // },
+  // {
+  //   id: 2,
+  //   url: "https://www.youtube.com/embed/jd_lEsHDirM?si=P8PiFDyHi5gDiivF",
+  //   title: "Persuratan Digital",
+  // },
+  // {
+  //   id: 2,
+  //   url: "https://www.youtube.com/embed/J84ZTPegk5U?si=Y_PDIJ6W5yrplZm8",
+  //   title: "Bakorwil III Malang",
+  // },
 ];
 
 export default portfolioData;
