23c6b416550c96adac0531a3d1339d02a53375dd
dewandra <75962411+dewandra@users.noreply.github.com>
2024-07-15 10:32:56 +0700

    Update portofolioData.js

--- src/component/portofolioData.js
+++ src/component/portofolioData.js
@@ -1,19 +1,24 @@
 const portfolioData = [
-  // {
-  //   id: 1,
-  //   url: "https://www.youtube.com/embed/9QHMsB4wAAs?si=polRgbezBH8Q61aO",
-  //   title: "Profile Website",
-  // },
-  // {
-  //   id: 2,
-  //   url: "https://www.youtube.com/embed/jd_lEsHDirM?si=P8PiFDyHi5gDiivF",
-  //   title: "Persuratan Digital",
-  // },
-  // {
-  //   id: 2,
-  //   url: "https://www.youtube.com/embed/J84ZTPegk5U?si=Y_PDIJ6W5yrplZm8",
-  //   title: "Bakorwil III Malang",
-  // },
+   {
+     id: 1,
+     url: "https://www.youtube.com/embed/L_ZQ7s7lVlI?si=lxLuBVCgN0RUCler",
+     title: "E-Commerce - Laravel",
+   },
+      {
+     id: 2,
+     url: "https://www.youtube.com/embed/lmWDygleIUU?si=ShP3rUnDvnB9hkS7",
+     title: "Community Web - Laravel",
+   },
+   {
+     id: 3,
+     url: "https://www.youtube.com/embed/jd_lEsHDirM?si=P8PiFDyHi5gDiivF",
+     title: "Persuratan Digital - React Js",
+   },
+   {
+     id: 4,
+     url: "https://www.youtube.com/embed/J84ZTPegk5U?si=Y_PDIJ6W5yrplZm8",
+     title: "Bakorwil III Malang - React Js",
+   },
 ];
 
 export default portfolioData;
